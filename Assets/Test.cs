53940f805b2a198d3e4f4ecd55de142b46ce972b
越 王 <27876367@qq.com>
2024-09-21 14:08:14 +0800

    delete test code

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+namespace A
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Test : MonoBehaviour
     {
-        
-    }
+        private cls a;
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            a = new cls();
+            a.name = "a";
+            UnityEngine.Debug.Log(a.name);
+        }
+    }
+}
+
+namespace A
+{
+    class cls
     {
-        UnityEngine.Debug.Log("B");
+        public string name;
     }
 }
